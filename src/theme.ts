97732a52ebb3f1fd9246caf77d7d41984f23a9d4
Rachel Werner <rachelannwerner@gmail.com>
2024-12-05 22:17:52 +0100

    refactoring code

--- src/theme.ts
+++ src/theme.ts
@@ -4,6 +4,22 @@ const config: ThemeConfig = {
   initialColorMode: 'dark'
 };
 
-const theme = extendTheme({ config });
+const theme = extendTheme({ 
+  config, 
+  colors: { 
+    gray:  {
+      50: '#eeeef6',
+      100: '#cdcce1',
+      200: '#ababcd',
+      300: '#8a89bc',
+      400: '#6967aa',
+      500: '#504e91',
+      600: '#3e3d70',
+      700: '#2d2c4f',
+      800: '#1b1a2f',
+      900: '#090911',
+    }} });
 
 export default theme;
+
+
